5bae6eb37840573a2b08dfc66e49e9fcfb5dc180
Pablo Encina <encinaacevedo.pablo@gmail.com>
2026-07-31 09:24:41 -0400

    feat: PWA (Android/Chrome/Iphone

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
     <html lang="es">
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/logo_black_512.png" />
+        <link rel="apple-touch-icon" href="/logo_white_512.png" />
         <meta name="theme-color" content="#111827" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
